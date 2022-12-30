9127c8f2ac6d2649446f9015b61534ec0e5debe3
Rohit Raj <rohit06092000@gmail.com>
2022-12-31 01:01:05 +0530

    Insertio

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,3 +1,12 @@
 class ISort:
     def InsertionSort( self,arr):
-        
+        for i in range(len(arr) - 1):
+            x = i + 1
+            for j in reversed(range(i+1)):
+                if arr[x] < arr[j]:
+                    arr[j],arr[x] = arr[x],arr[j]
+                    x = j
+        return arr
+a = ISort()
+arr = [5, -2, 23, 7, 87, -42, 509 ]
+print(a.InsertionSort(arr))
